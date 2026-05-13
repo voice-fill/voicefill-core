c0c57dec236e4264c42b646593f57ce0d3434994
amadejzr <amadej@jerlah.si>
2026-05-13 10:51:33 +0200

    feat: add public exports and verify build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export { createVoiceFill } from './client.js';
+export type { VoiceFillClient } from './client.js';
+export type {
+  VoiceFillConfig,
+  AudioInput,
+  TranscribeResult,
+  ExtractOptions,
+  FillOptions,
+  FillResult,
+} from './types.js';
